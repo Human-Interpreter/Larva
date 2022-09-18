a9015af250525e582ffaf4996c9914b997325ff4
YeJun, Jung <yejun614@naver.com>
2022-09-18 23:02:28 +0900

    Fix: CardObject 용도 변경

--- Larva/Assets/Scripts/Gameplay/Card/CardObject.cs
+++ Larva/Assets/Scripts/Gameplay/Card/CardObject.cs
@@ -1,27 +1,12 @@
 using UnityEngine;
-using Mirror;
 
 namespace Larva
 {
     /// <summary>
     /// 카드 게임 오브젝트
+    /// (로컬 플레이어 화면에 표시되는 조작 가능한 카드 오브젝트)
     /// </summary>
-    public class CardObject : NetworkBehaviour
+    public class CardObject : MonoBehaviour
     {
-        /// <summary>
-        /// 카드 데이터
-        /// </summary>
-        public CardData Data;
-
-        /// <summary>
-        /// 카드 소유 플레이어
-        /// </summary>
-        public Player Owner;
-
-        /// <summary>
-        /// 카드 액션 함수
-        /// </summary>
-        public virtual void CardAction()
-        {}
     }
 }
