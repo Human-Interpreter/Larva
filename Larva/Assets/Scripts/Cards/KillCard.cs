5e9b38de8e63a16e1e339034e2650c0d79923ffd
YeJun, Jung <yejun614@naver.com>
2022-09-26 20:54:39 +0900

    Feat: Larva 네임스페이스 추가

--- Larva/Assets/Scripts/Cards/KillCard.cs
+++ Larva/Assets/Scripts/Cards/KillCard.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillCard : MonoBehaviour
+namespace Larva
 {
-    // Start is called before the first frame update
-    void Start()
+    public class KillCard : MonoBehaviour
     {
-        
-    }
+        void Start()
+        {
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Update()
+        {
+            
+        }
     }
 }
+
