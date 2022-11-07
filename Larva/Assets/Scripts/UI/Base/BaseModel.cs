6b16ff8c1d202b03c16cf58d4d3ab36bde8bd6eb
Cardroid <number1dh@gmail.com>
2022-11-07 20:21:39 +0900

    Feat: 뷰 모델 베이스 클래스 작성

---
+++ Larva/Assets/Scripts/UI/Base/BaseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.UI.Base
+{
+    [Serializable]
+    public abstract class BaseModel : IModel, INotifyPropertyChanged, IEquatable<BaseModel>
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public virtual bool Equals(BaseModel other)
+        {
+            return object.ReferenceEquals(this, other);
+        }
+    }
+}
---
+++ Larva/Assets/Scripts/UI/Base/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Base
+{
+    public class BaseViewModel : MonoBehaviour, IViewModel, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
